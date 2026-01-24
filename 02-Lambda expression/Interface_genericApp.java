4269605dd8006efd4cf8c693d0191628e2715dc8
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-24 11:02:32 +0530

    JDK 1.8 Lambada Function Expression the interface with generics concept

---
+++ 02-Lambda expression/Interface_genericApp.java
@@ -0,0 +1,20 @@
+@FunctionalInterface
+interface Square<T>{
+	T get_Square(T no); // public abstract T get_Square();
+}
+
+public class Interface_genericApp{
+	public static void main(String x[]){
+		
+		/*	Method = I ( Anonymous inner class ) with generics 
+		
+		Square<Float>sq=new Square<Float>(){
+			public Float get_Square(Float no){
+				return no*no;
+			}
+		};
+		float res = sq.get_Square(5.5f);
+		System.out.println("Square is : "+res); // Square is : 30.25
+		*/
+	}
+}
