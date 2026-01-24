91fe97afb1c1fb57e6e0965a76c8df5423c621ac
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-24 11:27:14 +0530

    Runnable Functional Interface

---
+++ 03-Inbuilt Functional Interfaces/Threading_LambdaExpAPP.java
@@ -0,0 +1,67 @@
+
+public class Threading_LambdaExpAPP{
+	public static void main(String x[]){
+		/*	Method - I
+		
+		Runnable rn = new Runnable(){
+			public void run(){
+				try{
+					for(int i=1; i<=5; i++){
+						System.out.println("I = "+i);
+						Thread.sleep(1000);
+					}
+				}catch(Exception ex){
+					System.out.println("Error is : "+ex);
+				}
+			}
+		};
+		Thread t =new Thread(rn);
+		t.start();		
+		*/
+		
+		/*	Method - II
+		
+		Runnable rn = ()->{
+			try{
+					for(int i=1; i<=5; i++){
+						System.out.println("I = "+i);
+						Thread.sleep(1000);
+					}
+				}catch(Exception ex){
+					System.out.println("Error is : "+ex);
+				}	
+		};
+		Thread t =new Thread(rn);
+		t.start();
+		*/
+		
+		/*	Method - III
+		
+		Thread t =new Thread( ()->{
+			try{
+					for(int i=1; i<=5; i++){
+						System.out.println("I = "+i);
+						Thread.sleep(1000);
+					}
+				}catch(Exception ex){
+					System.out.println("Error is : "+ex);
+				}
+		});
+		t.start();
+		*/
+		
+		//	Method - IV
+		
+		new Thread(()->{
+			try{
+					for(int i=1; i<=5; i++){
+						System.out.println("I = "+i);
+						Thread.sleep(1000);
+					}
+				}catch(Exception ex){
+					System.out.println("Error is : "+ex);
+				}
+		}).start();
+		
+	}
+}
