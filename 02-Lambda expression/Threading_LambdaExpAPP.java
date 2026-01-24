c2c52da1efb70a5e630085790dffae3f3a1a9dbf
Yadnynesh Dhangar <yadnyneshdhangar@gmail.com>
2026-01-24 11:17:40 +0530

    Example of threading using Lambda expression

---
+++ 02-Lambda expression/Threading_LambdaExpAPP.java
@@ -0,0 +1,19 @@
+
+public class Threading_LambdaExpAPP{
+	public static void main(String x[]){
+		Runnable rn = new Runnable(){
+			public void run(){
+				try{
+					for(int i=1; i<=5; i++){
+						System.out.println("I = "+i);
+						Thread.sleep(1000);
+					}
+				}catch(Exception ex){
+					System.out.println("Error is : "+ex);
+				}
+			}
+		};
+		Thread t =new Thread(rn);
+		t.start();		
+	}
+}
