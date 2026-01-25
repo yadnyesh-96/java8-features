1ef18d55010b3c7af6d300ac9eca074d9931391a
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-01-25 22:08:52 +0530

    consumer interface in java

---
+++ 03-Inbuilt Functional Interfaces/2-Consumer Interface/ConsumerInterfaceApp.java
@@ -0,0 +1,14 @@
+
+java.util.function.*;
+public class ConsumerInterfaceApp{
+	public static void main(String x[]){
+		
+		Consumer<Integer> cm=new Consumer<Integer>(){
+			public void accept(Integer val){
+				System.out.println("VALUE :"+val);
+			}
+		};
+		
+		cm.accept(100);
+	}
+}
