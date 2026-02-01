be0142cb2437632a1028e359f20f646fff3cd33c
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 00:03:23 +0530

    Function Interface

--- 03-Inbuilt Functional Interfaces/4-Function Interface/FunctionInterfaceAPP.java
+++ 03-Inbuilt Functional Interfaces/4-Function Interface/FunctionInterfaceAPP.java
@@ -8,11 +8,18 @@ import java.util.function.*;
 
 public class FunctionInterfaceAPP{
 	public static void main(String x[]){
+		/*
 		Function<Integer,Integer> f = new Function<Integer,Integer>(){
 			public Integer apply(Integer val){
 				return val*val;
 			}
 		};
+		
+		Integer res = f.apply(10);
+		System.out.println("Square is :"+res);
+		*/
+		
+		Function<Integer,Integer> f =(Integer val)->val*val;
 		
 		Integer res = f.apply(10);
 		System.out.println("Square is :"+res);
