2d0ec1bc4197384ddcf69feab5f72814295e4769
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 00:42:11 +0530

    Supplier Interface

--- 03-Inbuilt Functional Interfaces/5-Supplier Interface/SupplierInterface.java
+++ 03-Inbuilt Functional Interfaces/5-Supplier Interface/SupplierInterface.java
@@ -3,13 +3,20 @@ import java.util.*;
 import java.util.function.*;
 public class SupplierInterface{
 	public static void main(String x[]){
-		
+		/*
 		Supplier<Date> s = new Supplier<Date>(){
 				public Date get(){
 					return new Date();
 				}
 		};
+		Date sysDate= s.get();
+		System.out.println(sysDate);
+		*/
 		
+		Supplier<Date> s = ()->get(){
+					return new Date();
+				}
+		};
 		Date sysDate= s.get();
 		System.out.println(sysDate);
 	}
