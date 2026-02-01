c1ebb29b0bac8a6c13aa7a5ea31726edd1b37d15
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-01 23:15:16 +0530

    public default Predicate<T> and(Predicate<? super T>):

--- 03-Inbuilt Functional Interfaces/3-Predicated Interface/PredicateInterface.java
+++ 03-Inbuilt Functional Interfaces/3-Predicated Interface/PredicateInterface.java
@@ -2,7 +2,7 @@
 import java.util.function.*;
 public class PredicateInterface{
 	public static void main(String x[]){
-		
+		/* 
 		Predicate<Integer> p1 = new Predicate<Integer>(){
 			public boolean test(Integer valFirst){
 				return valFirst>=10;
@@ -14,6 +14,19 @@ public class PredicateInterface{
 				return valSecond<=20;
 			}
 		};
+		
+		Predicate<Integer> p3 = p1.and(p2);
+		boolean res = p3.test(12);
+		if(res){
+			System.out.println("Value is greater than 10 and less than 20");
+		}else{
+			System.out.println("Value is less than equal 10 or greater than 20");
+		}
+		*/
+		
+		Predicate<Integer> p1 = (Integer valFirst)->valFirst>=10;
+		
+		Predicate<Integer> p2 =(Integer valSecond)->valSecond<=20;
 		
 		Predicate<Integer> p3 = p1.and(p2);
 		boolean res = p3.test(12);
