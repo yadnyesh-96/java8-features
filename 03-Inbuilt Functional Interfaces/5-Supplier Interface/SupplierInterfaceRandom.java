e0bb78deb434e37311e537012d6d896d8adf4897
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 00:48:06 +0530

    Supplier Interface and random number generate example

---
+++ 03-Inbuilt Functional Interfaces/5-Supplier Interface/SupplierInterfaceRandom.java
@@ -0,0 +1,15 @@
+
+import java.util.*;
+import java.util.function.*;
+public class SupplierInterfaceRandom{
+	public static void main(String x[]){
+		
+		Supplier<Integer> s = new Supplier<Integer>(){
+			public Integer get(){
+				return new Random().nextInt(100);
+			}
+		};
+		
+		System.out.println(s.get());
+	}
+}	
