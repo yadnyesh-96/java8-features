cb6cbca510ca0047c606162fd9ba95c5b4baab4c
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 00:12:47 +0530

    Function Interface with andThen() Method

---
+++ 03-Inbuilt Functional Interfaces/4-Function Interface/FunctionInterfaceandThen.java
@@ -0,0 +1,20 @@
+//Example: WAP to calculate square of 10 and multiply it by 100
+
+import java.util.*;
+import java.util.function.*;
+
+public class FunctionInterfaceandThen{
+	public static void main(String x[]){
+		Function<Integer,Integer> f =(Integer val)->val*val;
+		
+		Function<Integer,Integer> f1 = new Function<Integer,Integer>(){
+			public Integer apply(Integer res){
+				return res*100;
+			}
+		};
+		
+		Function<Integer,Integer> f2 = f.andThen(f1);
+		Integer result = f2.apply(10);
+		System.out.println("Final Result is:"+result);
+	}
+}
