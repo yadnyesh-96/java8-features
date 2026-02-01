fc8a1298484b7ff3ec4d7e69139215d3c01cbfa1
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-01 16:42:52 +0530

    predicated Interface in java

--- 03-Inbuilt Functional Interfaces/3-Predicated Interface/PredicateInterfaceAPP.java
+++ 03-Inbuilt Functional Interfaces/3-Predicated Interface/PredicateInterfaceAPP.java
@@ -1,14 +1,18 @@
-
+import java.util.function.*;
 import java.util.*;
 class PredicateInterfaceAPP{
 	public static void main(String x[]){
-		
+		/* Method - I
 		Predicate<Integer> p =new Predicate<>(){
 			public boolean test(Integer val){
 			 return val%2==0;
 			}
 		};
+		*/
 		
+		Predicate<Integer> p=(Integer val)->{
+				return val%2==0?true:false;
+		};
 		boolean res=p.test(10);
 		if(res){
 			System.out.println("Even Number");
