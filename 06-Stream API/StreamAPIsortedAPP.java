7e3818711239edc2ba2343f4292c757af190d9b4
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 22:13:00 +0530

    Refactor StreamAPIsortedAPP to simplify sorting example using method chaining

--- 06-Stream API/StreamAPIsortedAPP.java
+++ 06-Stream API/StreamAPIsortedAPP.java
@@ -4,9 +4,16 @@ import java.util.stream.*;
 public class StreamAPIsortedAPP{
 	public static void main(String x[]){
 		
+		/*
 		List<Integer> list =Arrays.asList(5,9,2,3,1);
 		Stream<Integer> s1 = list.stream();
 		Stream<Integer> s2 = s1.sorted();
 		s2.forEach((e)->System.out.println(e));	
+		*/
+		
+		Arrays.asList(5,9,2,3,1).
+			stream().
+				sorted().
+					forEach((e)->System.out.println(e));	
 	}
 }
