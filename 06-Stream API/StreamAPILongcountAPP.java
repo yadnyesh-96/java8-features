e68ff40664954f4393fd5ce2d505b8c1be908d48
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 22:54:16 +0530

    Add StreamAPILongcountAPP to demonstrate counting elements in a list using streams

---
+++ 06-Stream API/StreamAPILongcountAPP.java
@@ -0,0 +1,13 @@
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+public class StreamAPILongcountAPP{
+	public static void main(String x[]){
+		
+		List<Integer> ls = Arrays.asList(10,20,30,40,50);
+		Stream<Integer> s1 = ls.stream();
+		long c = s1.count();
+		System.out.println("Number of Element is::"+c);
+	}
+}
