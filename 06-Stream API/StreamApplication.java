2be917346ca7593cdab5bb9c174a38ec7b1b070d
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 17:27:36 +0530

    Stream API

---
+++ 06-Stream API/StreamApplication.java
@@ -0,0 +1,25 @@
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+public class StreamApplication{
+	public static void main(String x[]){
+		
+		List<Integer>list=List.of(10,20,30,40,50);
+		Stream<Integer> s = list.stream();
+		Function<Integer,Integer> s1 = new Function<Integer,Integer>(){
+				public Integer apply(Interger v){
+					return v*v;
+				}
+		};		
+		
+		Stream<Integer>s2=s1.map(s1);
+		Consumer<Integer> c = new Consumer<Integer>(){
+			public void accept(Integer v){
+				System.out.println(v);
+			}
+		};
+		
+		s2.forEach(c);
+	}
+}
