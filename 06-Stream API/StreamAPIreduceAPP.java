807be93e2615d7d2c1f71ad45d4f7754d48634e8
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 22:46:41 +0530

    Add StreamAPIreduceAPP to demonstrate reduction of elements in a list using streams

---
+++ 06-Stream API/StreamAPIreduceAPP.java
@@ -0,0 +1,21 @@
+
+import java.util.*;
+import java.util.function.*;
+import java.util.stream.*;
+
+public class StreamAPIreduceAPP{
+	public static void main(String x[]){
+		
+		List<Integer> ls =Arrays.asList(10,10,20,30,10,20,30,40,50,50,50);
+		Stream<Integer> s1 = ls.stream();
+		
+		BinaryOperator<Integer> b = new BinaryOperator<Integer>(){
+			public Integer apply(Integer val1, Integer val2){
+				return val1+val2;
+			}
+		};
+		
+		Integer res = s1.reduce(0,b);
+		System.out.println(res);
+	}
+}
