511e07f4b2e8e70edca569000dfa2df94c1389ac
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 22:41:32 +0530

    Add StreamAPIcollectAPP to demonstrate collecting distinct elements from a list using streams

--- 06-Stream API/StreamAPIcollectAPP.java
+++ 06-Stream API/StreamAPIcollectAPP.java
@@ -6,10 +6,18 @@ import java.util.stream.*;
 public class StreamAPIcollectAPP{
 	public static void main(String x[]){
 		
+		/*
 		List<Integer> list =Arrays.asList(10,10,20,30,10,20,30,40,50,50,50);
 		Stream<Integer> s1 = list.stream();
 		Stream<Integer> s2 = s1.distinct();
-		List<Integer> res = s2.collect(Collection.toList());
+		List<Integer> res = s2.collect(Collectors.toList());
 		res.forEach((val)->System.out.println(val));
+		*/
+		
+		Arrays.asList(10,10,20,30,10,20,30,40,50,50,50).
+			stream().
+				distinct().
+					collect(Collectors.toList()).
+						forEach((val)->System.out.println(val));
 	}
 }
