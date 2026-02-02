431825d7960084a8f9f872ad8c80281715f67b30
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 17:44:05 +0530

    Add StreamFilterApplication to demonstrate filtering with streams

--- 06-Stream API/StreamFilterApplication.java
+++ 06-Stream API/StreamFilterApplication.java
@@ -1,3 +1,4 @@
+// Example: WAP to create collection and store values in it and find the even values   and display it.
 
 import java.util.*;
 import java.util.function.*;
@@ -6,6 +7,7 @@ import java.util.stream.*;
 public class StreamFilterApplication{
 	public static void main(String x[]){
 		
+		/*
 		List<Integer> list=List.of(10,11,20,42,15,16,17);
 		Stream<Integer> s1 = list.stream();
 		Predicate<Integer> p = new Predicate<Integer>(){
@@ -13,6 +15,14 @@ public class StreamFilterApplication{
 				return val%2==0;
 			}
 		};
+		Stream<Integer> s2=s1.filter(p);
+		s2.forEach((val)->System.out.println(val));
+		*/
+		
+
+		Stream<Integer> s1 = List.of(10,11,20,42,15,16,17).stream();
+		Predicate<Integer> p =(Integer val)->val%2==0;
+
 		Stream<Integer> s2=s1.filter(p);
 		s2.forEach((val)->System.out.println(val));
 	}
