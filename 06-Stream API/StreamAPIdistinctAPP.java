051588da10f847354c85e15dba1925d81d2dc44d
yadnyesh-96 <yadnyneshdhangar@gmail.com>
2026-02-02 22:33:04 +0530

    Add StreamAPIdistinctAPP to demonstrate distinct elements in a list using streams

--- 06-Stream API/StreamAPIdistinctAPP.java
+++ 06-Stream API/StreamAPIdistinctAPP.java
@@ -6,10 +6,17 @@ import java.util.function.*;
 public class StreamAPIdistinctAPP{
 	public static void main(String x[]){
 		
-		List<Integer> ls=Array.asList(10,10,20,30,44,40,40,15,50,50,50);
+		/*
+		List<Integer> ls=Arrays.asList(10,10,20,30,44,40,40,15,50,50,50);
 		
 		Stream<Integer> s1 = ls.stream();
 		Stream<Integer> s2 = s1.distinct();
-		s2.forEach((val)->System.out.println(va));
+		s2.forEach((val)->System.out.println(val));
+		*/
+			
+		Arrays.asList(10,10,20,30,44,40,40,15,50,50,50).
+			stream().
+				distinct().
+					forEach((val)->System.out.println(val));
 	}
 }
